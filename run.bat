a001caf2a9f90f479a7b49eb1461c4ba1139395e
gc04caps <gc04caps@gmail.com>
2017-07-16 00:21:33 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 TITLE Screen Capture for Non-Programmers
 CD .//bin/
 ECHO Welcome to Screen Capture for Non-Programmers!
-ECHO Did you read the instructions and enter your username, password, etc?
+ECHO Did you read the instructions and enter your username, password, etc, and create the folders?
 PAUSE
 CMD /K  StreamCapture.exe
